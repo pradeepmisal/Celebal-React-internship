88276bf0b7feead44c95c92b3fc7039fbe0c8aea
pradeep misal <144873499+pradeepmisal@users.noreply.github.com>
2025-06-08 07:40:41 +0000

    nitialize React form validation application with routing and form handling

---
+++ react-form-validation/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Form from './components/Form';
+import Success from './components/Success';
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </Router>
+
+  );
+}
+export default App;
